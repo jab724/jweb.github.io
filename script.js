187d3ef17bfa36fcc25f3b7ec9a3093e08361e5a
jab724 <jab724@naver.com>
2020-01-06 11:17:12 +0900

    ss

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timerID;
 var colorValue = 255;
 var colorChangeSpeed = 10;
-// var myBody = document.querySelector('body');
+var myBody = document.querySelector('body');
 var backgroundChanger = {
     changeWhite: function () {
         if (colorValue >= 255) {
@@ -9,17 +9,17 @@ var backgroundChanger = {
             clearInterval(timerID);
             timerID = null;
             colorValue = 255;
-            // myBody.style.color = "rgb(0,0,0)"
-            // myBody.style.backgroundColor = "rgb(255,255,255)";
-            $('body').css('color', "rgb(0,0,0)");
-            $('body').css('backgroundColor', "rgb(255,255,255)");
+            myBody.style.color = "rgb(0,0,0)"
+            myBody.style.backgroundColor = "rgb(255,255,255)";
+            // $('body').css('color', "rgb(0,0,0)");
+            // $('body').css('backgroundColor', "rgb(255,255,255)");
         }
         else {
             colorValue = colorValue + (255 / 60) * colorChangeSpeed;
-            // myBody.style.color = "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")";
-            // myBody.style.backgroundColor = "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")";
-            $('body').css('color', "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")");
-            $('body').css('backgroundColor', "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")");
+            myBody.style.color = "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")";
+            myBody.style.backgroundColor = "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")";
+            // $('body').css('color', "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")");
+            // $('body').css('backgroundColor', "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")");
         }
 
     },
@@ -29,13 +29,17 @@ var backgroundChanger = {
             clearInterval(timerID);
             timerID = null;
             colorValue = 0;
-            $('body').css('color', "rgb(255,255,255)");
-            $('body').css('backgroundColor', "rgb(0,0,0)");
+            myBody.style.color = "rgb(255,255,255)"
+            myBody.style.backgroundColor = "rgb(0,0,0)";
+            // $('body').css('color', "rgb(255,255,255)");
+            // $('body').css('backgroundColor', "rgb(0,0,0)");
         }
         else {
             colorValue = colorValue - (255 / 60) * colorChangeSpeed;
-            $('body').css('color', "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")");
-            $('body').css('backgroundColor', "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")");
+            myBody.style.color = "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")";
+            myBody.style.backgroundColor = "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")";
+            // $('body').css('color', "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")");
+            // $('body').css('backgroundColor', "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")");
         }
     }
 }
@@ -45,16 +49,16 @@ function init()
     if(getCookie('isDarkMode') == '1'){
         // alert(getCookie('isDarkMode'));
         colorValue = 0;
-        $('modeChange').value = "일반으로 변경";
-        $('body').css('color', "rgb(255,255,255)");
-        $('body').css('backgroundColor', "rgb(0,0,0)");
+        document.querySelector('modeChange').value = "일반으로 변경";
+        myBody.style.color = "rgb(255,255,255)"
+        myBody.style.backgroundColor = "rgb(0,0,0)";
     }
     else{
         // alert(getCookie('isDarkMode'));
         colorValue = 255;
-        $('modeChange').value = "야간으로 변경";
-        $('body').css('color', "rgb(" + (255 - colorValue) + "," + (255 - colorValue) + "," + (255 - colorValue) + ")");
-        $('body').css('backgroundColor', "rgb(" + colorValue + "," + colorValue + "," + colorValue + ")");
+        document.querySelector('modeChange').value = "야간으로 변경";
+        myBody.style.color = "rgb(0,0,0)"
+        myBody.style.backgroundColor = "rgb(255,255,255)";
     }
 }
 
